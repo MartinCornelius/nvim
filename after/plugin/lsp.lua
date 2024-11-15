25b9bd1729dd6d7e4b069330ec9ce353e0932623
MartinCornelius <martincornelius.nielsen@gmail.com>
2024-11-15 19:09:08 +0100

    feat: added gr command for refactor

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -19,6 +19,7 @@ lsp.configure('phpactor', {
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'gr', function() vim.lsp.buf.rename() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
 	vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
