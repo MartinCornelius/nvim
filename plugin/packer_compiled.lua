134b5364a3149101df899d7703eca6b62f40421d
MartinCornelius <martincornelius.nielsen@gmail.com>
2024-11-21 16:01:44 +0100

    update

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -104,11 +104,6 @@ _G.packer_plugins = {
     path = "/home/cornelius/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
-  everforest = {
-    loaded = true,
-    path = "/home/cornelius/.local/share/nvim/site/pack/packer/start/everforest",
-    url = "https://github.com/sainnhe/everforest"
-  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/home/cornelius/.local/share/nvim/site/pack/packer/start/friendly-snippets",
@@ -133,6 +128,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/cornelius/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
+  },
+  ["null-ls.nvim"] = {
+    loaded = true,
+    path = "/home/cornelius/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
+    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
