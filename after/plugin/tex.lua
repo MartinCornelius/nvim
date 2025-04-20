393288b00c503fc1aec83883c602ac144548b3af
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2025-04-20 09:03:05 +0200

    fix: fix "-p" folder created on compiling tex files

--- after/plugin/tex.lua
+++ after/plugin/tex.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     pattern = "*.tex",
     callback = function()
         local out_dir = "out"
-        vim.cmd("silent !mkdir -p " .. out_dir)
+        vim.cmd("silent !mkdir " .. out_dir)
         vim.cmd("silent !latexmk -pdf -output-directory=" .. out_dir)
     end,
 })
