12510118f5e7073155ef78e9eb952d607a025dcd
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2025-12-22 21:10:41 +0100

    Update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,17 +1,8 @@
 require("lazy").setup({
-    { "rose-pine/neovim", name = "rose-pine", lazy = true },
     { "echasnovski/mini.pick", event = "VeryLazy" },
-    { "vague2k/vague.nvim", event = "BufWinEnter" },
     { "williamboman/mason.nvim", event = "BufReadPre" },
     { "neovim/nvim-lspconfig", event = "BufReadPre" },
-    { 
-        "martincornelius/reword.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("reword").setup({
-                persist = true,
-            })
-        end
-    },
     { "saghen/blink.cmp" },
+    { "vague2k/vague.nvim", event = "BufWinEnter" },
+    { "rose-pine/neovim", name = "rose-pine", lazy = true },
 })
