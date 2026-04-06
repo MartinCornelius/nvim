0a13a45867f69129781ed65cee6ef267842f2186
MartinCornelius <martincornelius.nielsen@gmail.com>
2026-04-06 10:50:41 +0200

    remove lazy plugin manager

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,1 @@
-require("lazy").setup({
-    { "echasnovski/mini.pick", event = "VeryLazy" },
-
-    { "williamboman/mason.nvim", event = "BufReadPre" },
-    { "neovim/nvim-lspconfig", event = "BufReadPre" },
-    { "saghen/blink.cmp" },
-    -- { "github/copilot.vim" },
-
-    { "ntpeters/vim-better-whitespace", event = "VeryLazy" },
-
-    { "projekt0n/github-nvim-theme", event = "bufWinEnter" },
-    { "folke/tokyonight.nvim", event = "bufWinEnter" },
 })
