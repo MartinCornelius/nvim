e0c872629194cbe0817207ff46f1527b9e616990
MartinCornelius <martincornelius.nielsen@gmail.com>
2026-04-27 09:46:57 +0200

    Add keymaps back for lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -7,6 +7,10 @@ require("mason").setup({})
 -- grx → run codelens
 -- gO → document symbols
 -- Ctrl-S in Insert mode → signature help
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
 --
 -- Diagnostics:
 -- <C-w>d: Opens the diagnostic message
